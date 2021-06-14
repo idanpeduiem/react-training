8093edd83ccde2c7435e938d7b83f36efccbcd6a
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2021-06-14 22:06:19 +0300

    gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import './App.css';
 import {Card, Grid, makeStyles, Table, TableBody, TableCell, TableContainer, TableRow} from "@material-ui/core";
 import {FilterEnum} from './features/tableFilters/filters';
